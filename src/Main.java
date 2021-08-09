00088d4c1ca35bb494b1f0ae7b96982a79251e1b
Klrushka <comeindonotbeshy@gmail.com>
2021-08-09 20:38:04 +0300

    <---- ^_^

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import task3.Sets;
+
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+
+        /*
+        Exercise 3: (1) Using Countries, fill a Set multiple times with the same data and verify
+        that the Set ends up with only one of each instance. Try this with HashSet,
+        LinkedHashSet, and TreeSet.
+         */
+
+        Sets sets = new Sets();
+
+        for (int i = 0; i < 5; i++) {
+            Sets.insertCountries(sets.getHashSet());
+            Sets.insertCountries(sets.getTreeSet());
+            Sets.insertCountries(sets.getLinkedHashSet());
+        }
+
+        System.out.println(Sets.ORIGINAL_LINKED_HASH_SET.equals(sets.getLinkedHashSet()));
+        System.out.println(Sets.ORIGINAL_TREE_SET.equals(sets.getTreeSet()));
+        System.out.println(Sets.ORIGINAL_HASH_SET.equals(sets.getHashSet()));
+
+
+        /*
         
+         */
+
+
     }
 }
---
+++ src/task3/Sets.java
@@ -0,0 +1,39 @@
+package task3;
+
+import java.util.*;
+
+public class Sets {
+
+    public static final Set<String> ORIGINAL_HASH_SET = new HashSet<>();
+    public static final Set<String> ORIGINAL_LINKED_HASH_SET = new LinkedHashSet<>();
+    public static final Set<String> ORIGINAL_TREE_SET = new TreeSet<>();
+
+    static {
+        for (int i = 0; i < Countries.DATA.length; i++) {
+            ORIGINAL_LINKED_HASH_SET.add(Countries.DATA[i][0] + " " + Countries.DATA[i][1]);
+        }
+        ORIGINAL_HASH_SET.addAll(ORIGINAL_LINKED_HASH_SET);
+        ORIGINAL_TREE_SET.addAll(ORIGINAL_LINKED_HASH_SET);
+
+    }
+
+    private Set<String> hashSet = new HashSet<>();
+    private Set<String> linkedHashSet = new LinkedHashSet<>();
+    private Set<String> TreeSet = new TreeSet<>();
+
+    public static void insertCountries(Set<String> set) {
+        set.addAll(ORIGINAL_LINKED_HASH_SET);
+    }
+
+    public Set<String> getHashSet() {
+        return hashSet;
+    }
+
+    public Set<String> getLinkedHashSet() {
+        return linkedHashSet;
+    }
+
+    public Set<String> getTreeSet() {
+        return TreeSet;
+    }
+}
